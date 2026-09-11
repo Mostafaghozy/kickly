0e3645540faf29ef320a0778a0c4491e03cb5ddb
Mostafa <your_email@example.com>
2026-09-11 23:49:57 +0300

    feat:complete login UI

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
       themeMode: ThemeMode.system,
-      home: const LoginView(),
+      home: const PaymentMethod(),
     );
   }
 }
